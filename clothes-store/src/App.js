4e445ce028e4652153c2276f6b7881f955afdae4
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-09-29 13:41:36 +0300

    adding header componanet and links to All page shop , home , contanet

--- clothes-store/src/App.js
+++ clothes-store/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import Homepage from './pages/home/Homepage' 
-import './App.scss'
-import { Route, Switch } from 'react-router'  
+import React from "react";
+import Homepage from "./pages/home/Homepage";
+import "./App.scss";
+import { Route, Switch } from "react-router";
 
-import ShopPage from './pages/shop/ShopPage'
-
-
-
-
-
-
+import ShopPage from "./pages/shop/ShopPage";
+import Header from "./componantes/header/Header";
 
 export default function App() {
- 
   return (
-   <Switch>
-   
+    <div>
 
-   <Route exact path='/' component={Homepage} />
-   <Route  exact path='/shop' component={ShopPage} /> 
+      <Header/> 
+     
+      <Switch>
 
-   </Switch>
-  )
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+     
+      </Switch>
+    </div>
+  );
 }
